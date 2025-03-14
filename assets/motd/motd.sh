67e0fd945685fef81a1007bea7da33d80c7e6c7e
Malsoryz <ikmalalansory@gmail.com>
2025-03-14 22:34:12 +0800

    Motd Update 2025-03-14 22:34:12

--- assets/motd/motd.sh
+++ assets/motd/motd.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
-(cat $HOME/Public/git/Malsoryz/assets/motd/welcome.txt;
+(cat ./welcome.txt;
 echo " GitHub : https://github.com/Malsoryz";
 echo " Date   : $(date '+%A %d %B %Y %T')"; 
 echo " Host   : $(whoami)@$(hostname)";
